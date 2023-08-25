aec87038013e0dc5f92695f0e4508e68dcef9728
serabraa <96802108+serabraa@users.noreply.github.com>
2023-08-25 17:25:02 +0400

    Initial Commit

---
+++ Watchacuyc/WatchacuycApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchacuycApp.swift
+//  Watchacuyc
+//
+//  Created by Sergey on 25.08.23.
+//
+
+import SwiftUI
+
+@main
+struct WatchacuycApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
